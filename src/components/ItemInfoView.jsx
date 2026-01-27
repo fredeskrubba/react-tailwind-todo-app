9e2a064179fbb28806fa38b2be2113ccaf94f7d1
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-01-27 15:08:55 +0100

    add placeholder

--- src/components/ItemInfoView.jsx
+++ src/components/ItemInfoView.jsx
@@ -6,7 +6,7 @@ const ItemInfoView = ({ item, activeDescription, setActiveDescription }) => {
         <div className="fixed inset-y-0 right-0 w-[320px] bg-white border-l border-main-green p-6 overflow-y-auto shadow-lg z-50 md:flex-col md:justify-between hidden md:flex">
                         <div className='flex flex-col gap-4'>
                             <h2 className="text-xl font-semibold text-gray-900 mb-2">{item.title}</h2>
-                            <textarea className="w-full text-base text-gray-900 bg-transparent border-0 focus:outline-none focus:ring-0 resize-none" value={activeDescription} onChange={(e) => setActiveDescription(e.target.value)} />
+                            <textarea className="w-full text-base text-gray-900 bg-transparent border-0 focus:outline-none focus:ring-0 resize-none" value={activeDescription} onChange={(e) => setActiveDescription(e.target.value)} placeholder='Enter description'/>
 
                         </div>
                         <div className='flex flex-col border-t-1 border-main-green pt-4 gap-4'>
